45af452d7d6ed07837565be725b58b9619bbd21a
snake <lib0707@gmail.com>
2015-07-08 17:54:52 +0800

    03 - Data Model

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -12,6 +12,19 @@ class ChecklistViewController: UITableViewController {
     
     var items: [ChecklistItem]
     
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        item.checked = false
+        items.append(item)
+        
+        let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+    }
+    
     required init(coder aDecoder: NSCoder) {
         items = [ChecklistItem]()
         
@@ -75,6 +88,13 @@ class ChecklistViewController: UITableViewController {
             configureCheckmarkForCell(cell, withChecklistItem: item)
         }
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        items.removeAtIndex(indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
     }
     
     func configureCheckmarkForCell(cell: UITableViewCell, withChecklistItem item: ChecklistItem) {
