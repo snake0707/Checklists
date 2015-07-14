2f12bc214c2a11fa54bed7596221e5e7d7665c56
snake <lib0707@gmail.com>
2015-07-14 20:52:12 +0800

    Extending the data model

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -21,6 +21,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let controller = navigationController.viewControllers[0] as! AllListsViewController
         controller.dataModel = dataModel
         
+        let notificationSettings = UIUserNotificationSettings(forTypes: .Alert | .Sound, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        
+//        let date = NSDate(timeIntervalSinceNow: 10)
+//        
+//        let localNotification = UILocalNotification()
+//        localNotification.fireDate = date
+//        localNotification.timeZone = NSTimeZone.defaultTimeZone()
+//        localNotification.alertBody = "I am a local notification!"
+//        localNotification.soundName = UILocalNotificationDefaultSoundName
+//        
+//        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        
         return true
     }
 
@@ -51,7 +64,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func saveData() {
         dataModel.saveChecklists()
     }
-
-
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        println("didReceiveLocalNotification \(notification)")
+    }
 }
 
